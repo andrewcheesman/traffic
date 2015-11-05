88552fd56902481fadc65ab9ff338dc7c713fdf3
Andrew Cheesman <andrewcheesman@gmail.com>
2015-11-05 20:10:02 +0000

    Updated TODO

--- _TODO.R
+++ _TODO.R
@@ -1,5 +1,7 @@
 # Open Items
 # - Explore layers and groups in leaflet; may be able to update colors instead of re-mapping
+# - Set CRON to copy auto-generated files (on Monday mornings) to Shiny server directory
+
 
 # Items complete as of 11/5
 # - Eliminate graphing.R and roll into mapping.R
